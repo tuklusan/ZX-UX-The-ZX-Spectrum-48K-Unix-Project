a6047c07a9e0e6e6a8bfb6be8a40c3498e9d0962
Supratim Sanyal <tuklusan@users.noreply.github.com>
2026-09-22 16:34:40 -0400

    P8.31: finalize uname qualification source

--- v1/src/utils/uname.asm
+++ v1/src/utils/uname.asm
@@ -10,6 +10,7 @@
 ; SANYALnet Labs." See LICENSE for full terms, warranty disclaimer, termination,
 ; patent, trademark, and governing-law provisions.
 
+; P8.31 exact qualification source.
     MACRO EMIT_P831_UNAME_ROUTINES
 uname_entry:
     push hl
