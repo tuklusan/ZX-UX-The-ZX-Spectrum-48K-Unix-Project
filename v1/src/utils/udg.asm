1dbe2bbe1950f7f4938966f78ae568200932b636
Supratim Sanyal <tuklusan@users.noreply.github.com>
2026-09-22 13:13:21 -0400

    P8.21: add udg list and show command forms

--- v1/src/utils/udg.asm
+++ v1/src/utils/udg.asm
@@ -425,3 +425,304 @@ udg_statout: defs 10,0
 udg_payload:
     defs UDG1_FULL_SIZE,0
     ENDM
+
+
+; P8.21 public udg command wrapper. The frozen P7.16 save/load implementation
+; remains byte-for-byte reusable; this wrapper adds list/show and dispatches all
+; four architecture-defined forms from a single MEX1 entry at offset zero.
+    MACRO EMIT_P821_UDG_ROUTINES
+    jp udg_p821_entry
+    EMIT_P716_UDG_UTILITY_ROUTINES
+
+udg_p821_entry:
+    push hl
+    pop ix
+    ld a,(ix+4)
+    cp 2
+    jr z,udg_p821_two
+    cp 3
+    jr z,udg_p821_three
+    jp udg_p821_bad
+
+udg_p821_two:
+    ld de,8
+    add ix,de
+udg_p821_skip0_two:
+    ld a,(ix+0)
+    inc ix
+    or a
+    jr nz,udg_p821_skip0_two
+    ld a,(ix+0)
+    cp 'l'
+    jr nz,udg_p821_bad
+    ld a,(ix+1)
+    cp 'i'
+    jr nz,udg_p821_bad
+    ld a,(ix+2)
+    cp 's'
+    jr nz,udg_p821_bad
+    ld a,(ix+3)
+    cp 't'
+    jr nz,udg_p821_bad
+    ld a,(ix+4)
+    or a
+    jr nz,udg_p821_bad
+    call udg_p821_list
+    jr udg_p821_exit
+
+udg_p821_three:
+    ; save/load retain the certified P7.16 entry contract.
+    push ix
+    pop hl
+    ld de,8
+    add hl,de
+udg_p821_skip0_probe:
+    ld a,(hl)
+    inc hl
+    or a
+    jr nz,udg_p821_skip0_probe
+    ld a,(hl)
+    cp 's'
+    jp z,udg_entry
+    cp 'l'
+    jp z,udg_entry
+    cp 's'
+    jr z,udg_p821_bad
+    ; The only remaining legal three-argument verb is exact lower-case show.
+    ld a,(hl)
+    cp 's'
+    jr nz,udg_p821_bad
+    inc hl
+    ld a,(hl)
+    cp 'h'
+    jr nz,udg_p821_bad
+    inc hl
+    ld a,(hl)
+    cp 'o'
+    jr nz,udg_p821_bad
+    inc hl
+    ld a,(hl)
+    cp 'w'
+    jr nz,udg_p821_bad
+    inc hl
+    ld a,(hl)
+    or a
+    jr nz,udg_p821_bad
+    inc hl
+    call udg_p821_parse_slot
+    jr c,udg_p821_exit
+    ld (udg_p821_slot),a
+    ld c,a
+    ld b,0
+    ld hl,udg_p821_glyph
+    ld a,SYS_UDG_GET
+    call SYSCALL_GATEWAY
+    jr c,udg_p821_exit
+    call udg_p821_show
+    jr udg_p821_exit
+
+udg_p821_bad:
+    ld a,E_INVAL
+    scf
+udg_p821_exit:
+    jr nc,udg_p821_exit_ok
+    ld l,a
+    jr udg_p821_do_exit
+udg_p821_exit_ok:
+    ld l,0
+udg_p821_do_exit:
+    ld h,0
+    ld a,SYS_EXIT
+    call SYSCALL_GATEWAY
+    ret
+
+; HL=NUL decimal, exact 0..31, no sign/leading junk.
+udg_p821_parse_slot:
+    ld a,(hl)
+    cp '0'
+    jr c,udg_p821_parse_bad
+    cp '9'+1
+    jr nc,udg_p821_parse_bad
+    sub '0'
+    ld b,a
+    inc hl
+    ld a,(hl)
+    or a
+    jr z,udg_p821_parse_one
+    cp '0'
+    jr c,udg_p821_parse_bad
+    cp '9'+1
+    jr nc,udg_p821_parse_bad
+    sub '0'
+    ld c,a
+    inc hl
+    ld a,(hl)
+    or a
+    jr nz,udg_p821_parse_bad
+    ld a,b
+    cp 3
+    jr c,udg_p821_parse_tens
+    jr nz,udg_p821_parse_bad
+    ld a,c
+    cp 2
+    jr nc,udg_p821_parse_bad
+    ld a,30
+    add a,c
+    or a
+    ret
+udg_p821_parse_tens:
+    add a,a
+    ld d,a
+    add a,a
+    add a,a
+    add a,d
+    add a,c
+    or a
+    ret
+udg_p821_parse_one:
+    ld a,b
+    or a
+    ret
+udg_p821_parse_bad:
+    ld a,E_INVAL
+    scf
+    ret
+
+udg_p821_list:
+    xor a
+    ld (udg_p821_slot),a
+udg_p821_list_loop:
+    ld a,(udg_p821_slot)
+    call udg_p821_write_slot_number
+    ret c
+    ld hl,udg_p821_lf
+    ld bc,1
+    call udg_p821_write_all
+    ret c
+    ld a,(udg_p821_slot)
+    inc a
+    ld (udg_p821_slot),a
+    cp UDG1_MAX_GLYPHS
+    jr nz,udg_p821_list_loop
+    xor a
+    ret
+
+udg_p821_show:
+    ld a,(udg_p821_slot)
+    call udg_p821_write_slot_number
+    ret c
+    ld hl,udg_p821_colon
+    ld bc,2
+    call udg_p821_write_all
+    ret c
+    ld hl,udg_p821_glyph
+    ld b,8
+udg_p821_show_loop:
+    ld a,(hl)
+    inc hl
+    push hl
+    push bc
+    call udg_p821_write_hex
+    pop bc
+    pop hl
+    ret c
+    djnz udg_p821_show_loop
+    ld hl,udg_p821_lf
+    ld bc,1
+    jp udg_p821_write_all
+
+; A=0..31 decimal, no leading zero.
+udg_p821_write_slot_number:
+    cp 10
+    jr c,udg_p821_one_digit
+    ld c,0
+udg_p821_tens_loop:
+    sub 10
+    inc c
+    cp 10
+    jr nc,udg_p821_tens_loop
+    ld b,a
+    ld a,c
+    add a,'0'
+    ld (udg_p821_num),a
+    ld a,b
+    add a,'0'
+    ld (udg_p821_num+1),a
+    ld hl,udg_p821_num
+    ld bc,2
+    jp udg_p821_write_all
+udg_p821_one_digit:
+    add a,'0'
+    ld (udg_p821_num),a
+    ld hl,udg_p821_num
+    ld bc,1
+    jp udg_p821_write_all
+
+udg_p821_write_hex:
+    ld c,a
+    rrca
+    rrca
+    rrca
+    rrca
+    and $0f
+    call udg_p821_hex_nibble
+    ld (udg_p821_hex),a
+    ld a,c
+    and $0f
+    call udg_p821_hex_nibble
+    ld (udg_p821_hex+1),a
+    ld hl,udg_p821_hex
+    ld bc,2
+    jp udg_p821_write_all
+udg_p821_hex_nibble:
+    cp 10
+    jr c,udg_p821_hex_digit
+    add a,'A'-10
+    ret
+udg_p821_hex_digit:
+    add a,'0'
+    ret
+
+udg_p821_write_all:
+    ld (udg_p821_wptr),hl
+    ld (udg_p821_wleft),bc
+udg_p821_write_loop:
+    ld hl,(udg_p821_wptr)
+    ld bc,(udg_p821_wleft)
+    ld de,1
+    ld a,SYS_WRITE
+    call SYSCALL_GATEWAY
+    ret c
+    ld a,h
+    or l
+    jr z,udg_p821_write_zero
+    ld (udg_p821_wrote),hl
+    ld de,(udg_p821_wptr)
+    add hl,de
+    ld (udg_p821_wptr),hl
+    ld hl,(udg_p821_wleft)
+    ld de,(udg_p821_wrote)
+    or a
+    sbc hl,de
+    jr c,udg_p821_write_zero
+    ld (udg_p821_wleft),hl
+    ld a,h
+    or l
+    jr nz,udg_p821_write_loop
+    xor a
+    ret
+udg_p821_write_zero:
+    ld a,E_IO
+    scf
+    ret
+
+udg_p821_slot: db 0
+udg_p821_glyph: defs 8,0
+udg_p821_num: defs 2,0
+udg_p821_hex: defs 2,0
+udg_p821_colon: db ':',' '
+udg_p821_lf: db 10
+udg_p821_wptr: dw 0
+udg_p821_wleft: dw 0
+udg_p821_wrote: dw 0
+    ENDM
