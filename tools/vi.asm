04873f3c91c575cd495b812d12d2f3f235c5f65a
Supratim Sanyal <tuklusan@users.noreply.github.com>
2026-09-22 20:17:23 -0400

    P9.02: implement typed buffer loader with transient source handle

--- tools/vi.asm
+++ tools/vi.asm
@@ -162,4 +162,138 @@ vi_have_cursor: db 0
 vi_primary_errno: db 0
 vi_restore_errno: db 0
 vi_work: db 0
+
+; P9.02 named-buffer loader.  The source handle is transient: a completed
+; load has no live source handle before any editor command can be accepted.
+VI_LOAD_CAPACITY        EQU 1024
+
+vi_p902_load:
+    ld (vi_load_path),hl
+    xor a
+    ld (vi_source_open),a
+    ld (vi_buffer_ready),a
+    ld (vi_buffer_len),a
+    ld (vi_buffer_len+1),a
+
+    ld (vi_stat_req),hl
+    ld hl,vi_stat_out
+    ld (vi_stat_req+2),hl
+    ld hl,vi_stat_req
+    ld a,SYS_STAT
+    call SYSCALL_GATEWAY
+    ret c
+
+    ld a,(vi_stat_out)
+    cp OBJ_TXT
+    jr z,vi_load_type_ok
+    cp OBJ_ASM
+    jr z,vi_load_type_ok
+    cp OBJ_C
+    jr z,vi_load_type_ok
+    cp OBJ_CFG
+    jr z,vi_load_type_ok
+    ld a,E_FORMAT
+    scf
+    ret
+
+vi_load_type_ok:
+    ld hl,(vi_load_path)
+    ld c,O_READ
+    ld b,0
+    ld a,SYS_OPEN
+    call SYSCALL_GATEWAY
+    ret c
+    ld a,h
+    or a
+    jr nz,vi_load_bad_handle
+    ld a,l
+    ld (vi_source_handle),a
+    ld a,1
+    ld (vi_source_open),a
+
+vi_load_read:
+    ld a,(vi_source_handle)
+    ld e,a
+    ld d,0
+    ld hl,vi_io_chunk
+    ld bc,64
+    ld a,SYS_READ
+    call SYSCALL_GATEWAY
+    jr c,vi_load_stream_error
+    ld a,h
+    or l
+    jr z,vi_load_eof
+    ld (vi_chunk_len),hl
+    ex de,hl
+    ld hl,(vi_buffer_len)
+    add hl,de
+    ld a,h
+    cp 4
+    jr c,vi_load_room
+    jr nz,vi_load_overflow
+    ld a,l
+    or a
+    jr nz,vi_load_overflow
+vi_load_room:
+    ld (vi_new_len),hl
+    ld de,vi_buffer
+    ld hl,(vi_buffer_len)
+    add hl,de
+    ex de,hl
+    ld hl,vi_io_chunk
+    ld bc,(vi_chunk_len)
+    ldir
+    ld hl,(vi_new_len)
+    ld (vi_buffer_len),hl
+    jr vi_load_read
+
+vi_load_overflow:
+    ld a,E_NOMEM
+    jr vi_load_stream_error
+
+vi_load_eof:
+    call vi_close_source
+    ret c
+    ld a,1
+    ld (vi_buffer_ready),a
+    xor a
+    ret
+
+vi_load_stream_error:
+    ld (vi_load_errno),a
+    call vi_close_source
+    ld a,(vi_load_errno)
+    scf
+    ret
+
+vi_load_bad_handle:
+    ld a,E_FORMAT
+    scf
+    ret
+
+vi_close_source:
+    ld a,(vi_source_open)
+    or a
+    ret z
+    xor a
+    ld (vi_source_open),a
+    ld a,(vi_source_handle)
+    ld l,a
+    ld h,0
+    ld a,SYS_CLOSE
+    call SYSCALL_GATEWAY
+    ret
+
+vi_load_path: dw 0
+vi_source_handle: db 0
+vi_source_open: db 0
+vi_buffer_ready: db 0
+vi_load_errno: db 0
+vi_chunk_len: dw 0
+vi_new_len: dw 0
+vi_buffer_len: dw 0
+vi_stat_req: defs 4,0
+vi_stat_out: defs 10,0
+vi_io_chunk: defs 64,0
+vi_buffer: defs VI_LOAD_CAPACITY,0
     ENDM
